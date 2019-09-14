11b540a17e4a8592301e1d037c04241ab0efc9eb
Jiatao Zhang <1193744197@qq.com>
2019-09-14 17:02:50 +0800

    change param

--- run/run.py
+++ run/run.py
@@ -46,7 +46,7 @@ parser.add_argument("--verbose", help="print detailed info for debugging",
                     action="store_true")
 parser.add_argument('-m', '--model', type=str, default='rect', help="choose model ('logi' or 'rect'). default: rect")
 parser.add_argument('-d', '--dim', type=int, default=128, help="set dimension. default: 128")
-parser.add_argument('--epoch', type=int, default=100, help="set number of epochs. default: 100")
+parser.add_argument('--epoch', type=int, default=300, help="set number of epochs. default: 100")
 parser.add_argument('--lr', type=float, default=0.001, help="set learning rate. default: 0.001")
 parser.add_argument('--batch_size', type=int, default=1024, help="set batch size. default: 1024")
 parser.add_argument('--n_neg', type=int, default=10, help="Number of negative samples per (h,r,t). default: 10")
